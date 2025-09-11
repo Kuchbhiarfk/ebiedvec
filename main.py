c722a8078d26e083fe3b2638c336bed046b8b56c
Kuchbhiarfk <cha.r.dl.o.o62@googlemail.com>
2025-09-11 06:55:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -862,4 +862,5 @@ def reset_and_set_commands():
 if __name__ == "__main__":
     reset_and_set_commands()
     notify_owner()
-    bot.run(resume_tasks(bot))  # Modified to include task resumption
+
+bot.run()
